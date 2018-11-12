87d6294d248cde66f2c23bbfd5370b772c219ff6
Carl Boettiger <cboettig@gmail.com>
2018-11-12 14:30:04 -0800

    drop various experimental scripts
    all this is now in the Windows installer methods

--- inst/windows/virtuoso_setup.cmd
+++
@@ -1,9 +0,0 @@
-set VIRTUOSO_HOME=C:\Program Files\OpenLink Software\Virtuoso OpenSource 7.20
-set PATH=%PATH%;%VIRTUOSO_HOME%/bin;%VIRTUOSO_HOME%/lib
-
-
-REM "REM" (Remark) is the comment character in cmd scripts; the below is commented out
-REM cd %VIRTUOSO_HOME%/lib
-REM regsvr32 virtodbc.dll
-
-
