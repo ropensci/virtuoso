59d03628a326ae77817a31b41808373f7785defc
Carl Boettiger <cboettig@gmail.com>
2018-11-24 21:58:03 -0800

    guess extension

--- R/vos_import.R
+++ R/vos_import.R
@@ -7,11 +7,17 @@
 #' @param wd The directory from which we can search the files.
 #' NOTE: This directory must match or be in the directory from which you ran vos_start().
 #' This is the default behavior and probably best not altered.
-#' @param ext glob and extension to match file types. Defaults to match any nquads.
+#' @param ext glob and extension to match file types. Will guess based on files specified
 #' @param graph Name (technically URI) for a graph in the database.  Can leave as default.
 #'
 #' @export
-vos_import <- function(con, files, wd = ".", ext = "*.nq", graph = "rdflib"){
+vos_import <- function(con, files, wd = ".", ext = NULL, graph = "rdflib"){
+
+  stopifnot(all(assert_extensions(files))) # could be more helpful error
+  if (is.null(ext)) ext <- guess_ext(files)
+
+
+
   ## We have to copy files into the directory Virtuoso can access.
   ## This is the directory where virtuoso.ini is located.
   if(!dir.exists(wd)) dir.create(wd)
@@ -22,6 +28,8 @@ vos_import <- function(con, files, wd = ".", ext = "*.nq", graph = "rdflib"){
   if(is_windows()) wd <- normalizePath(wd, winslash = "/")
 
   DBI::dbGetQuery(con, paste0("ld_dir('", wd, "', '", ext, "', '", graph, "')") )
+
+  ## Can call loader multiple times on multicore to load multiple files...
   DBI::dbGetQuery(con, "rdf_loader_run()" )
 
   ## clean up
@@ -31,3 +39,27 @@ vos_import <- function(con, files, wd = ".", ext = "*.nq", graph = "rdflib"){
   invisible(files)
 }
 
+assert_extensions <- function(files){
+  known_extensions <- c("grdf", "nq",  "owl", "nt",
+                        "rdf", "trig", "ttl", "xml")
+  pattern <- paste0("[.]", known_extensions, "(.gz)?$")
+  results <-
+    vapply(files, function(filename) any(
+      vapply(pattern, grepl, logical(1L), filename)),
+      logical(1L)
+    )
+
+  invisible(results)
+}
+
+
+guess_ext <- function(files){
+  filename <- basename(files[[1]])
+  ext <- sub(".*([.]\\w+)", "*\\1", filename)
+  if(ext == "*.gz"){
+    ext <- paste0(sub(".*([.]\\w+)", "*\\1",
+                      sub("[.]\\w+$", "", filename)),
+                  ".gz")
+  }
+  ext
+}
---
+++ tests/testthat/test-utilities.R
@@ -0,0 +1,14 @@
+context("utilities")
+
+test_that("we can detect valid and invalid file types", {
+  files <- c("test.nq", "test2.nq.gz", "stuff.tar.gz")
+  results <- assert_extensions(files)
+  expect_false(results[[3]])
+  expect_true(results[[2]])
+  })
+
+test_that("we can guess extension", {
+
+  expect_equal(guess_ext("test.nq"), "*.nq")
+  expect_equal(guess_ext("test.nq.gz"), "*.nq.gz")
+})
