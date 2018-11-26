addf890bb9149a2d18b1f61f7fe777528a2945cb
Carl Boettiger <cboettig@gmail.com>
2018-11-26 09:43:33 -0800

    typo

--- R/vos_import.R
+++ R/vos_import.R
@@ -7,7 +7,7 @@
 #' @param wd The directory from which we can search the files.
 #' NOTE: This directory must match or be in the directory from which you ran vos_start().
 #' This is the default behavior and probably best not altered.
-#' @param glob A wildcard aka globbing pattern (e.g. *.nq).
+#' @param glob A wildcard aka globbing pattern (e.g. "*.nq").
 #' @param graph Name (technically URI) for a graph in the database.  Can leave as default.
 #' If a graph is already specified by the import file (e.g. in nquads), that will be used
 #' instead.
@@ -21,20 +21,18 @@ vos_import <- function(con, files = NULL, wd = ".", glob = "*", graph = "rdflib"
   ## If given a list of specific files
 
   stopifnot(all(assert_extensions(files))) # could be more helpful error
-  if (is.null(ext)) ext <- guess_ext(files)
-
 
 
   ## We have to copy files into the directory Virtuoso can access.
   ## This is the directory where virtuoso.ini is located.
   if(!dir.exists(wd)) dir.create(wd)
   ## Can we use file.symlink instead of copy?
-  lapply(files, function(from) file.copy(from, file.path(wd, basename(from))))
+  lapply(files, function(from) file.symlink(from, file.path(wd, basename(from))))
 
   ## Even on Windows, ld_dir wants a Unix-style path-slash
   if(is_windows()) wd <- normalizePath(wd, winslash = "/")
 
-  DBI::dbGetQuery(con, paste0("ld_dir('", wd, "', '", ext, "', '", graph, "')") )
+  DBI::dbGetQuery(con, paste0("ld_dir('", wd, "', '", glob, "', '", graph, "')") )
 
   ## Can call loader multiple times on multicore to load multiple files...
   DBI::dbGetQuery(con, "rdf_loader_run()" )
