f56368048dc8d8257ca8f78931a10971a544400d
Carl Boettiger <cboettig@gmail.com>
2018-12-03 22:55:29 -0800

    a bit more disciplined path configuration
    - Allows paths to be set as env vars, fall back to rappdirs paths.  (we use all of rappdirs dirs! log, cache, config, data).
    - ensure paths exist
    
    A few unrelated changes:
    - drop export of questionable function
    - add a destroy method to clean paths

--- R/virtuoso.R
+++ R/virtuoso.R
@@ -1,19 +1,9 @@
-
-#' Run SPARQL query
-#'
-#' @param query a SPARQL query statement
-#' @inheritParams vos_import
-#' @export
-vos_query <- function(con, query, graph = "rdflib"){
-  ## FIXME construct SQL query more nicely with existing parsers
-  DBI::dbGetQuery(con, paste0("SPARQL ",  query))
-}
+## Some possible helper routines for common requests.
 
 #' Clear all triples from a graph
 #'
 #' @details NOTE: after clearing a graph, re-running the bulk importer may not
 #' re-import triples, at least until virtuoso-server is restarted.
-#' @export
 #' @inheritParams vos_import
 vos_clear_graph <- function(con, graph = "rdflib"){
   DBI::dbGetQuery(con, paste0("SPARQL CLEAR GRAPH <", graph, ">"))
@@ -43,15 +33,17 @@ vos_list_graphs <- function(con){
 )
 }
 
-vos_count_triples <- function(con){
-  ## Official queries, not sure why these return large negative integer
+
+## Not working...
+#' count triples
+#'
+#' @inheritParams vos_import
+vos_count_triples <- function(con, graph = NULL){
+  ## Official queries, not sure why these return large negative integer on debian and fail on mac...
   #DBI::dbGetQuery(con, "SPARQL SELECT COUNT(*) FROM <rdflib>")
   #DBI::dbGetQuery(con, "SPARQL SELECT (COUNT(?s) AS ?triples) WHERE { GRAPH ?g { ?s ?p ?o } }")
 
-  ## Seems to be correct total, but not the number of distinct
-  DBI::dbGetQuery(con, "SPARQL SELECT DISTINCT (COUNT(?s) AS ?triples) WHERE { ?s ?p ?o }")
-
-  ## Return all triples and count by graph in R. Must fit in memory :(
+  ## Or the dplyr way:
   ## df <- DBI::dbGetQuery(con, "SPARQL SELECT ?g ?s ?p ?o  WHERE { GRAPH ?g {?s ?p ?o} }")
   ## dplyr::count_(df, "g")
 }
