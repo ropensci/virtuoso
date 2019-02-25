c7169dc167739d0769d4ef15551bd5e53984fcbd
Carl Boettiger <cboettig@gmail.com>
2019-02-25 21:18:51 +0000

    ...

--- tests/testthat/setup-virtuoso.R
+++ tests/testthat/setup-virtuoso.R
@@ -1,8 +1,12 @@
 
+
+testthat::setup({
 message("Running one-time install of Virtuoso for tests...")
 wait <- !has_virtuoso()
 
-if (identical(Sys.getenv("NOT_CRAN"), "true"))  vos_install(ask = FALSE)
+# testthat::skip_on_cran()
+# if (identical(Sys.getenv("NOT_CRAN"), "true"))
+  vos_install(ask = FALSE)
 if (wait) Sys.sleep(20)
 
-
+})
