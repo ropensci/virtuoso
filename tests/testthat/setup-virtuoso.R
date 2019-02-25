9dd8c0e4e52a82848703fc5aa8f7a4cf6bad9a70
Carl Boettiger <cboettig@gmail.com>
2019-02-25 20:32:55 +0000

    appveyor :fire:

--- tests/testthat/setup-virtuoso.R
+++ tests/testthat/setup-virtuoso.R
@@ -4,9 +4,8 @@ testthat::setup({
   message("Running one-time install of Virtuoso for tests...")
   wait <- !has_virtuoso()
 
-  testthat::skip_on_cran()
-  #if (identical(Sys.getenv("NOT_CRAN"), "true"))
-    vos_install(ask = FALSE)
+  #testthat::skip_on_cran()
+  vos_install(ask = FALSE)
   if (wait) Sys.sleep(20)
 
 })
