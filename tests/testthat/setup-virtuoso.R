f3715d20bab3c675ebb81ff35a9143acd311c8c0
Carl Boettiger <cboettig@gmail.com>
2019-02-25 20:01:17 +0000

    do not attempt to install virtuoso server on CRAN

--- tests/testthat/setup-virtuoso.R
+++ tests/testthat/setup-virtuoso.R
@@ -1,6 +1,8 @@
 
 message("Running one-time install of Virtuoso for tests...")
-
 wait <- !has_virtuoso()
-vos_install(ask = FALSE)
+
+if (identical(Sys.getenv("NOT_CRAN"), "true"))
+  vos_install(ask = FALSE)
 if (wait) Sys.sleep(20)
+
