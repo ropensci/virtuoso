c5c876c83eeec34ce48f13fbc7c1af95c5bc7f42
Carl Boettiger <cboettig@gmail.com>
2019-02-25 20:57:06 +0000

    in which we manually tell Appveyor it's not CRAN
    Thanks to the brilliant @ldecicco-USGS for this solution! :sparkles:

--- tests/testthat/setup-virtuoso.R
+++ tests/testthat/setup-virtuoso.R
@@ -4,7 +4,7 @@ testthat::setup({
   message("Running one-time install of Virtuoso for tests...")
   wait <- !has_virtuoso()
 
-  #testthat::skip_on_cran()
+  testthat::skip_on_cran()
   vos_install(ask = FALSE)
   if (wait) Sys.sleep(20)
 
