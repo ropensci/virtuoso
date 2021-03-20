d2661733d08ddf54c520ee512ae47de8d939cb75
Carl <cboettig@gmail.com>
2021-03-20 16:18:04 +0000

    skip checks on GH-Actions for Windows
    (virtuoso installation not configured)

--- tests/testthat/test-without-vos.R
+++ tests/testthat/test-without-vos.R
@@ -2,7 +2,9 @@ context("tests that do not need a server connection")
 
 test_that("vos_process errors when not cached", {
   skip_on_os("solaris")
-  suppressMessages(vos_kill())
+  tryCatch(suppressMessages(vos_kill()),
+           error = function(e) NULL,
+           finally = NULL)
   expect_message(vos_kill(), "No active virtuoso")
   expect_null(vos_status())
   expect_identical(vos_process(), NA)
